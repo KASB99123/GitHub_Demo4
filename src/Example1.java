e15cf93f7b09bc97185ddfb7199b6812b923ff3b
suresh <sureshbabuk9912@gmail.com>
2023-02-06 19:02:01 +0530

    Example1 file completed and this is my commint02

---
+++ src/Example1.java
@@ -0,0 +1,14 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+
+		// sample text
+	  System.out.println(" Hi this is Example1 file , statement-1");
+	  System.out.println(" Hi this is Example1 file , statement-2");
+		
+		
+		
+	}
+
+}
